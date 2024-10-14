ec5544397230116a755dc7a1a68f55b8f3097d4b
RadBear <nbhung71711@gmail.com>
2024-10-14 10:11:55 +0700

    1

--- rcore/Assets/rabear.rcore/Runtime/Framework/Data/DateTimeData.cs
+++ rcore/Assets/rabear.rcore/Runtime/Framework/Data/DateTimeData.cs
@@ -2,35 +2,33 @@
  * Author RadBear - nbhung71711@gmail.com - 2018
  **/
 
+using RCore.Common;
 using System;
-using UnityEngine;
+using Debug = UnityEngine.Debug;
 
 namespace RCore.Framework.Data
 {
     public class DateTimeData : FunData
     {
-        private DateTime? m_value;
-        private DateTime? m_defaultValue;
-        private DateTime? m_compareValue; //If value is changed inside it, like AddDays or AddSeconds
+        private int m_value;
+        private int m_defaultValue;
         private bool m_changed;
-
-        public DateTime? Value
-        {
-            get => m_value;
-            set
-            {
-                if (value != m_value)
-                {
-                    m_value = value;
-                    m_compareValue = value;
-                    m_changed = true;
-                }
-            }
-        }
 
         public DateTimeData(int pId, DateTime? pDefaultValue = null) : base(pId)
         {
-            m_defaultValue = pDefaultValue;
+            if (pDefaultValue != null)
+                m_defaultValue = TimeHelper.DateTimeToUnixTimestampInt(pDefaultValue.Value);
+        }
+
+        public DateTime Get() => TimeHelper.UnixTimeStampToDateTime(m_value);
+
+        public void Set(DateTime time)
+        {
+            int timestamp = time.ToUnixTimestampInt();
+            if (timestamp == m_value)
+                return;
+            m_value = timestamp;
+            m_changed = true;
         }
 
         public override void Load(string pBaseKey, string pSaverIdString)
@@ -38,112 +36,86 @@ namespace RCore.Framework.Data
             base.Load(pBaseKey, pSaverIdString);
 
             m_value = GetSavedValue();
-            m_compareValue = m_value;
         }
 
         public void AddYears(int pYears)
         {
-            if (m_value == null)
-                return;
-            m_value.Value.AddYears(pYears);
+            m_value = TimeHelper.UnixTimeStampToDateTime(m_value).AddYears(pYears).ToUnixTimestampInt();
             m_changed = true;
         }
 
         public void AddMonths(int pMonths)
         {
-            if (m_value == null)
-                return;
-            m_value.Value.AddMonths(pMonths);
+            m_value = TimeHelper.UnixTimeStampToDateTime(m_value).AddMonths(pMonths).ToUnixTimestampInt();
             m_changed = true;
         }
 
         public void AddDays(int pDays)
         {
-            if (m_value == null)
-                return;
-            m_value.Value.AddDays(pDays);
+            m_value = TimeHelper.UnixTimeStampToDateTime(m_value).AddDays(pDays).ToUnixTimestampInt();
             m_changed = true;
         }
 
         public void AddHours(int pHours)
         {
-            if (m_value == null)
-                return;
-            m_value.Value.AddHours(pHours);
+            m_value = TimeHelper.UnixTimeStampToDateTime(m_value).AddHours(pHours).ToUnixTimestampInt();
             m_changed = true;
         }
 
         public void AddMinutes(int pMinutes)
         {
-            if (m_value == null)
-                return;
-            m_value.Value.AddMinutes(pMinutes);
+            m_value = TimeHelper.UnixTimeStampToDateTime(m_value).AddMinutes(pMinutes).ToUnixTimestampInt();
             m_changed = true;
         }
 
         public void AddSeconds(int pSeconds)
         {
-            if (m_value == null)
-                return;
-            m_value.Value.AddSeconds(pSeconds);
+            m_value = TimeHelper.UnixTimeStampToDateTime(m_value).AddSeconds(pSeconds).ToUnixTimestampInt();
             m_changed = true;
         }
 
         public override bool Stage()
         {
-            if (m_compareValue != m_value || m_changed)
+            if (m_changed)
             {
-                SetStringValue(m_value == null ? "" : m_value.Value.ToString());
-                m_compareValue = m_value;
+                SetStringValue(m_value.ToString());
                 m_changed = false;
                 return true;
             }
             return false;
         }
 
-        private DateTime? GetSavedValue()
+        private int GetSavedValue()
         {
             string val = GetStringValue();
             if (string.IsNullOrEmpty(val))
                 return m_defaultValue;
-
-            var output = DateTime.MinValue;
-            if (DateTime.TryParse(val, out output))
-            {
+            if (int.TryParse(val, out int output))
                 return output;
-            }
-            else
-            {
-                Debug.LogError("can not parse key " + m_Key + " with value " + val + " to int");
-                return m_defaultValue;
-            }
+            Debug.LogError($"can not parse key {m_Key} with value {val}");
+            return m_defaultValue;
         }
 
         public override void Reload()
         {
             base.Reload();
             m_value = GetSavedValue();
-            m_compareValue = m_value;
             m_changed = false;
         }
 
         public override void Reset()
         {
-            Value = m_defaultValue;
+            m_value = m_defaultValue;
         }
 
         public override bool Cleanable()
         {
-            if (m_Index != -1 && Value == m_defaultValue)
-            {
-                return true;
-            }
-            return false;
+            return m_Index != -1 && m_value == m_defaultValue;
         }
 
         public bool IsNullOrEmpty()
         {
-            return Value == null || Value.ToString() == "";
+            return m_value == 0;
         }
     }
 }

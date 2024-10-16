08fb78da44f3cbaa626539598fa8f44c04466cfa
RadBear <nbhung71711@gmail.com>
2024-10-16 17:17:34 +0700

    1

---
+++ rcore/Assets/PlayerPrefsEditor/Editor/PreferencesEditor/PreferenceEntryHolder.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace BgTools.PlayerPrefsEditor
+{
+    [System.Serializable]
+    public class PreferenceEntryHolder : ScriptableObject
+    {
+        public List<PreferenceEntry> userDefList;
+        public List<PreferenceEntry> unityDefList;
+
+        private void OnEnable()
+        {
+            hideFlags = HideFlags.DontSave;
+            userDefList ??= new List<PreferenceEntry>();
+            unityDefList ??= new List<PreferenceEntry>();
+        }
+
+        public void ClearLists()
+        {
+            if (userDefList != null)
+                userDefList.Clear();
+            if (unityDefList != null)
+                unityDefList.Clear();
+        }
+    }
+}
